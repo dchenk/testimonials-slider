46696cdfeded4ed0fed25c405be8c123c6a60f25
dchenk <dcherchenko@gmail.com>
2018-09-19 17:31:21 -0700

    basic theme customizations

--- restoringorder.php
+++ restoringorder.php
@@ -8,3 +8,38 @@ Author: Wider Webs (Dmitriy)
 Author URI: https://widerwebs.com
 License: Private
 */
+
+// Hide the "projects" custom post type created by the Divi theme.
+function remove_divi_projects($args) {
+	return array_merge($args, [
+		'public'             => false,
+		'publicly_queryable' => false,
+		'show_in_nav_menus'  => false,
+		'show_ui'            => false
+	]);
+}
+add_filter('et_project_posttype_args', 'remove_divi_projects', 10, 1);
+
+// Create a shortcode for displaying the previous/next links on testimonial posts.
+function testimonial_nav_links() {
+	return '<div class="testimonial-nav"><span class="nav-next">' .
+		get_next_post_link('%link', 'Next Testimonial <span style="font-family: ETmodules !important;">&#x3d;</span>', true) .
+		'</span><span class="nav-previous">' .
+		get_previous_post_link('%link', '<span style="font-family: ETmodules !important;">&#x3c;</span> Previous Testimonial', true) .
+		'</span></div>';
+}
+add_shortcode('testimonial_nav_links', 'testimonial_nav_links');
+
+// Woocommerce breadcrumbs home URL.
+function woo_custom_breadcrumb_home_url() {
+	return '/shop/';
+}
+add_filter('woocommerce_breadcrumb_home_url', 'woo_custom_breadcrumb_home_url');
+
+function woocommerce_change_breadcrumb_home_text($defaults) {
+	if (is_product_category()) {
+		$defaults['home'] = 'Shop';
+	}
+	return $defaults;
+}
+add_filter('woocommerce_breadcrumb_defaults', 'woocommerce_change_breadcrumb_home_text');
