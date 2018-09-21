665d8df8960dc912c49cc87514cfe6cbe87b8cba
dchenk <dcherchenko@gmail.com>
2018-09-21 11:52:56 -0700

    improved slider

--- testimonials-slider.js
+++ testimonials-slider.js
@@ -1,4 +1,20 @@
 (function() {
-	const wrappers = jQuery(".testimonials-slider");
-	console.log(wrappers);
+	jQuery(".testimonials-slider").each(function(_, wrapper) {
+		const slide = function(lr, list) {
+			const width = this.getBoundingClientRect().width;
+			const listLeft = list.getBoundingClientRect().left;
+			var listWidth = 0;
+			jQuery(list).children().each(function(_, elem) {
+				listWidth += jQuery(elem).outerWidth(true);
+			});
+			// const marginLeft = list.style.
+			if (lr === "left") {
+			} else {
+				console.log("right")
+			}
+		};
+		const list = wrapper.querySelector(".testimonials-slider-list");
+		wrapper.querySelector(".testimonials-slider-left").addEventListener("click", slide.bind(wrapper, "left", list));
+		wrapper.querySelector(".testimonials-slider-right").addEventListener("click", slide.bind(wrapper, "right", list));
+	})
 })();
